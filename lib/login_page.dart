cd49644fb074a7e7bf2e2ad43cbd991bd8757241
Evina <64392104+Evina08@users.noreply.github.com>
2021-05-15 21:56:52 +0700

    sign in with google

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,6 +9,31 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<UserCredential> googleSignIn() async {
+    GoogleSignIn googleSignIn = GoogleSignIn();
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    if (googleUser != null) {
+      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+      if (googleAuth.idToken != null && googleAuth.accessToken != null) {
+        final AuthCredential credential = GoogleAuthProvider.credential(
+            accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+
+        final UserCredential user =
+            await _auth.signInWithCredential(credential);
+
+        await Navigator.pushReplacementNamed(context, "/");
+
+        return user;
+      } else {
+        throw StateError('Missing Google Auth Token');
+      }
+    } else
+      throw StateError('Sign in Aborted');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,6 +107,8 @@ class _LoginPageState extends State<LoginPage> {
               ],
             ),
             SizedBox(height: 20.0),
+            SignInButton(Buttons.Google,
+                text: "Sign up with Google", onPressed: googleSignIn)
           ],
         ),
       ),
