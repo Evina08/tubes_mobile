87bbe68dbdf09013274b41b96a2a114c94d18350
Evina <64392104+Evina08@users.noreply.github.com>
2021-05-15 21:43:06 +0700

    halaman awal (ui)

---
+++ lib/login_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 35.0),
+            Container(
+              height: 400,
+              child: Image(
+                image: AssetImage("images/start.jpg"),
+                fit: BoxFit.contain,
+              ),
+            ),
+            SizedBox(height: 20),
+            RichText(
+                text: TextSpan(
+                    text: 'Welcome to ',
+                    style: TextStyle(
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                    children: <TextSpan>[
+                  TextSpan(
+                      text: ' Admin',
+                      style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.orange))
+                ])),
+            SizedBox(height: 10.0),
+            Text(
+              'Pendataan Anggota dan Buku Perpustakaan Kota Malang',
+              style: TextStyle(color: Colors.black),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                    padding: EdgeInsets.only(left: 30, right: 30),
+                    onPressed: null,
+                    child: Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    color: Colors.orange),
+                SizedBox(width: 20.0),
+                RaisedButton(
+                    padding: EdgeInsets.only(left: 30, right: 30),
+                    onPressed: null,
+                    child: Text(
+                      'REGISTER',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    color: Colors.orange),
+              ],
+            ),
+            SizedBox(height: 20.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
